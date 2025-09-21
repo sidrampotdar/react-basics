c4612ba8ba61246433e75a2bdf67675bcaa02522
Sidramappa Potdar <126233261+sidrampotdar@users.noreply.github.com>
2025-09-21 22:25:01 +0530

    Card Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,9 @@
-import Header from "./Header.jsx";
-import Footer from "./footer.jsx";
-import Food from "./Food.jsx"
-
+import Card from './card.jsx'
 function App() {
   return (
-    <>
-      <Header />
-      <Food/>
-      <Footer />
-
-      
-    </>
+   
+    <Card/>
+   
   );
 }
 
