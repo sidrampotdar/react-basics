0c15b2cebf84caf12d12fc67aeeeeba0ec2a81d4
Sidramappa Potdar <126233261+sidrampotdar@users.noreply.github.com>
2025-09-22 10:12:29 +0530

    Props and  Proptypes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
-import Button from "./Button.jsx";
-
+import Student from './Student.jsx'
 function App() {
-  
+  // props = read-only properties that are shared between components.
+// A parent component can send data to a child component.
+// <Component key=value />
   return(
-    <Button/>
+   <>
+   <Student name="SpongeBob" age={25} isStudent={true}/>
+   <Student name="SpongeBob" age={42} isStudent={false}/>
+     <Student name="Squidward" age={50} isStudent={false}/>
+       <Student name="Sandy" age={27} isStudent={true}/>
+   </>
   );
 
 }
